66a45fa86c65e9523ee62529cb6f2b278573b78d
Briyanes <briyankrnd@gmail.com>
2026-06-17 07:01:52 +0700

    fix: increase marketplace icon size to h-6 for better visibility

--- src/components/payment/MarketplaceLinks.tsx
+++ src/components/payment/MarketplaceLinks.tsx
@@ -48,7 +48,7 @@ export function MarketplaceLinks({ links }: { links: MarketLinks }) {
               style={{ background: '#03AC0E' }}
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/icons/tokopedia.png" alt="Tokopedia" width={20} height={20} className="shrink-0" />
+              <img src="/icons/tokopedia.png" alt="Tokopedia" className="h-6 w-auto shrink-0 object-contain" />
               Beli di Tokopedia
             </a>
           )}
@@ -61,7 +61,7 @@ export function MarketplaceLinks({ links }: { links: MarketLinks }) {
               style={{ background: '#EE4D2D' }}
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/icons/shopee.png" alt="Shopee" width={20} height={20} className="shrink-0" />
+              <img src="/icons/shopee.png" alt="Shopee" className="h-6 w-auto shrink-0 object-contain" />
               Beli di Shopee
             </a>
           )}
